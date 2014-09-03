6073a31ed2804a8c780c92e0de8a987f64dfc45e
Earl Ferguson <ejferg@earls-mbp.attlocal.net>
2014-09-03 02:36:39 -0400

    Created error class

--- typings/parse/parse.d.ts
+++ typings/parse/parse.d.ts
@@ -11,7 +11,7 @@ declare module Parse {
 
     var applicationId: string;
     var javaScriptKey: string;
-    var masterKey: boolean;
+    var masterKey: string;
     var serverURL: string;
     var VERSION: string;
 
@@ -891,6 +891,72 @@ declare module Parse {
         function run<T>(name: string, data?: any, options?: ParseDefaultOptions): Promise<T>;
 
         function useMasterKey(): void;
+    }
+
+
+    class Error {
+
+        code: ErrorCode;
+        message: string;
+
+        constructor(code: ErrorCode, message: string);
+
+    }
+
+    enum ErrorCode {
+
+        OTHER_CAUSE = -1,
+        INTERNAL_SERVER_ERROR = 1,
+        CONNECTION_FAILED =  100,
+        OBJECT_NOT_FOUND =  101,
+        INVALID_QUERY =  102,
+        INVALID_CLASS_NAME =  103,
+        MISSING_OBJECT_ID =  104,
+        INVALID_KEY_NAME =  105,
+        INVALID_POINTER =  106,
+        INVALID_JSON =  107,
+        COMMAND_UNAVAILABLE =  108,
+        NOT_INITIALIZED =  109,
+        INCORRECT_TYPE =  111,
+        INVALID_CHANNEL_NAME =  112,
+        PUSH_MISCONFIGURED =  115,
+        OBJECT_TOO_LARGE =  116,
+        OPERATION_FORBIDDEN =  119,
+        CACHE_MISS =  120,
+        INVALID_NESTED_KEY =  121,
+        INVALID_FILE_NAME =  122,
+        INVALID_ACL =  123,
+        TIMEOUT =  124,
+        INVALID_EMAIL_ADDRESS =  125,
+        MISSING_CONTENT_TYPE =  126,
+        MISSING_CONTENT_LENGTH =  127,
+        INVALID_CONTENT_LENGTH =  128,
+        FILE_TOO_LARGE =  129,
+        FILE_SAVE_ERROR =  130,
+        FILE_DELETE_ERROR =  153,
+        DUPLICATE_VALUE =  137,
+        INVALID_ROLE_NAME =  139,
+        EXCEEDED_QUOTA =  140,
+        SCRIPT_FAILED =  141,
+        VALIDATION_ERROR =  142,
+        INVALID_IMAGE_DATA =  150,
+        UNSAVED_FILE_ERROR =  151,
+        INVALID_PUSH_TIME_ERROR = 152,
+        USERNAME_MISSING =  200,
+        PASSWORD_MISSING =  201,
+        USERNAME_TAKEN =  202,
+        EMAIL_TAKEN =  203,
+        EMAIL_MISSING =  204,
+        EMAIL_NOT_FOUND =  205,
+        SESSION_MISSING =  206,
+        MUST_CREATE_USER_THROUGH_SIGNUP =  207,
+        ACCOUNT_ALREADY_LINKED =  208,
+        LINKED_ID_MISSING =  250,
+        INVALID_LINKED_SESSION =  251,
+        UNSUPPORTED_SERVICE =  252,
+        AGGREGATE_ERROR = 600,
+        FILE_READ_ERROR = 601,
+        X_DOMAIN_REQUEST = 602
     }
 
     /**
